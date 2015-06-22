baf5633c1c6d637d7e90502aae0afdc43a9873cd
Salih Tolga Ozaslan <ozaslan@seas.upenn.edu>
2015-06-22 01:19:37 -0400

    -- Updated PID Controller node. -- Added wrapper class. -- Updated node to use the class, upgraded from C style to C++.

--- pid_cont/include/pid_cont.hh
+++ pid_cont/include/pid_cont.hh
@@ -1,16 +1,30 @@
 #ifndef _PID_CONT_HH_
 #define _PID_CONT_HH_
 
+#include <cmath>
+#include <iostream>
 #include <ros/ros.h>
 #include <Eigen/Dense>
+
+#include <utils.hh>
 #include <com_msgs/PDCmd.h>
 #include <cont_msgs/Heading.h>
 #include <cont_msgs/ContParams.h>
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <utilities.hh>
 
+class PIDController{
+private:
+  cont_msgs::PIDParams _params;
+  ros::Time _prev_time;
+	Eigen::Vector3d _int_err;
+  double _prev_thrust;
+public:
+  PIDController(): _int_err(0, 0, 0), _prev_thrust(0){}
+  com_msgs::PDCmd generate_command(const cont_msgs::Heading &heading);
+  void set_params(const cont_msgs::PIDParams &params);
+  const cont_msgs::PIDParams & get_params();
+};
+
+/*
 double mass, g;
 double kp_roll , kd_roll;
 double kp_pitch, kd_pitch;
@@ -38,5 +52,6 @@ int loop(const ros::NodeHandle &n);
 void heading_callback(const cont_msgs::Heading &msg);
 void cont_params_callback(const cont_msgs::ContParams &msg);
 int calculate_control_parameters();
+*/
 
 #endif
---
+++ pid_cont/src/pid_cont.cc
@@ -0,0 +1,112 @@
+#include "pid_cont.hh"
+
+com_msgs::PDCmd PIDController::generate_command(const cont_msgs::Heading &heading){
+	// Message to return
+	com_msgs::PDCmd pdcmd_msg;
+	ros::Time curr_time = heading.header.stamp;
+
+	double dt = ((double)curr_time.sec  - _prev_time.sec ) + 
+				((double)curr_time.nsec - _prev_time.nsec) / 1e9;
+	_prev_time = curr_time;
+	
+	Eigen::Vector4d quat;	// misc. variables	
+	Eigen::Vector3d rpy;	// ''	 ''
+
+	// Convert current orientation to Euler angles
+	quat(0) = heading.quat_from.w;
+	quat(1) = heading.quat_from.x;
+	quat(2) = heading.quat_from.y;
+	quat(3) = heading.quat_from.z;
+	rpy = utils::trans::quat2rpy(quat);	
+	double psi_from = rpy(2);
+	// Conver goal orientation to Euler angles
+	quat(0) = heading.quat_to.w;
+	quat(1) = heading.quat_to.x;
+	quat(2) = heading.quat_to.y;
+	quat(3) = heading.quat_to.z;
+	rpy = utils::trans::quat2rpy(quat);	
+	float psi_to = rpy(2);
+
+	// Constrain the maximum yaw speed.
+	double dpsi = utils::fix_angle(psi_to - psi_from) / dt;
+	dpsi = utils::clamp(dpsi, -_params.max_dyaw, _params.max_dyaw);
+	psi_to = utils::fix_angle(psi_from + dt * dpsi);
+
+	// Initialize intermediate variables
+	Eigen::Vector3d acc_to, vel_to  , pos_to, 
+					vel_from, pos_from;
+	Eigen::Vector3d acc_des, euler_des;
+	pos_from(0) = heading.pos_from.x;	
+	pos_from(1) = heading.pos_from.y;
+	pos_from(2) = heading.pos_from.z;
+	vel_from(0) = heading.vel_from.x;	
+	vel_from(1) = heading.vel_from.y;
+	vel_from(2) = heading.vel_from.z;
+
+	pos_to(0) = heading.pos_to.x;	
+	pos_to(1) = heading.pos_to.y;	
+	pos_to(2) = heading.pos_to.z;	
+	vel_to(0) = heading.vel_to.x;	
+	vel_to(1) = heading.vel_to.y;
+	vel_to(2) = heading.vel_to.z;	
+	acc_to(0) = heading.acc_to.x;	
+	acc_to(1) = heading.acc_to.y;	
+	acc_to(2) = heading.acc_to.z;	
+
+	// ### I still do not use KI
+	// Calculate the required acceleration and orientation
+	acc_des(0) = acc_to(0) + _params.kd_x * (vel_to(0) - vel_from(0)) + _params.kp_x * (pos_to(0) - pos_from(0)) + _params.ki_x * _int_err(0);
+	acc_des(1) = acc_to(1) + _params.kd_y * (vel_to(1) - vel_from(1)) + _params.kp_y * (pos_to(1) - pos_from(1)) + _params.ki_y * _int_err(1);
+	acc_des(2) = acc_to(2) + _params.kd_z * (vel_to(2) - vel_from(2)) + _params.kp_z * (pos_to(2) - pos_from(2)) + _params.ki_z * _int_err(2);
+
+	_int_err += (pos_to - pos_from) * dt;
+
+	// Limit the accelerations and rotational speeds
+	if(acc_des.norm() > _params.max_acc)
+		acc_des *= _params.max_acc / acc_des.norm();
+
+	euler_des(0) = (1/_params.g) * (acc_des(0) * sin(psi_from) - acc_des(1) * cos(psi_from));
+	euler_des(1) = (1/_params.g) * (acc_des(0) * cos(psi_from) + acc_des(1) * sin(psi_from));
+	euler_des(2) = psi_to;
+	// Saturate resultant inputs
+	euler_des(0) = utils::clamp(euler_des(0), -_params.max_tilt, _params.max_tilt);
+	euler_des(1) = utils::clamp(euler_des(1), -_params.max_tilt, _params.max_tilt);
+
+	// Convert accelation to motor input (thrust := grams)
+	double thrust = _params.mass * (acc_des(2)) * 1000;
+	thrust = thrust < 1 ? 1 : thrust;
+	// Prevent sudden step-ups
+	double dthrust = (thrust - _prev_thrust) / dt;
+	
+	if(dthrust >= 0 && dthrust >= _params.max_dthrust)
+		thrust = _prev_thrust + dt * _params.max_dthrust;
+	if(thrust > _params.max_thrust)
+		thrust = _params.max_thrust;
+	_prev_thrust = thrust;
+
+	// Pass the inputs to the driver side
+	pdcmd_msg.thrust	= thrust;
+	pdcmd_msg.roll		= euler_des(0);
+	pdcmd_msg.pitch		= euler_des(1);
+	pdcmd_msg.yaw		= euler_des(2);
+	pdcmd_msg.droll		= 0;
+	pdcmd_msg.dpitch	= 0;
+	pdcmd_msg.dyaw		= 0;
+	pdcmd_msg.kp_roll 	= _params.kp_roll;
+	pdcmd_msg.kd_roll 	= _params.kd_roll;
+	pdcmd_msg.kp_pitch	= _params.kp_pitch;
+	pdcmd_msg.kd_pitch	= _params.kd_pitch;
+	pdcmd_msg.kp_yaw 	= _params.kp_yaw;
+	pdcmd_msg.kd_yaw 	= _params.kd_yaw;
+
+	return pdcmd_msg;
+}
+
+void PIDController::set_params(const cont_msgs::PIDParams &params){
+	_params = params;
+}
+
+const cont_msgs::PIDParams & PIDController::get_params(){
+	return _params;
+}
+
